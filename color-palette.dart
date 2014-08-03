375ea415c24662ee874bd6cc9e8a52f2a90fe256
David Ramalho <dramalho@gmail.com>
2014-08-03 18:02:02 +0100

    Separate ColorPalette from rest of code

--- color-palette.dart
+++ color-palette.dart
@@ -25,7 +25,6 @@ class ColorPalette {
 
     for (var i = 0; i < d.length; i += 4) {
       var idx = (d[i] + d[i+1] + d[i+2]);
-      // var idx = d[i] + d[i+1] + d[i+2];
 
       if (m[idx] == null) {
         m[idx] = {'rgb': [d[i],d[i+1],d[i+2]], 'count': 0};
@@ -43,58 +42,4 @@ class ColorPalette {
 
     return m;
   }
-}
-
-void main() {
-  var img = querySelector('.orig');
-  window.onLoad.listen((e) {
-    processPalette(img);
-  });
-}
-
-List sortColorMapByRGB(Map colorMap) {
-  var list = colorMap.values.toList();
-  list.sort( (x, y) => x['rgb'].reduce((a,b) => a + b).compareTo(y['rgb'].reduce((a,b) => a + b)));
-
-  return list;
-}
-
-List sortColoMapByFrequency(Map colorMap) {
-  var list = colorMap.values.toList();
-  list.sort( (x, y) => x['count'].compareTo(y['count']));
-
-  return list;
-}
-
-void transformImage(ImageElement img) {
-  CanvasElement canvas = querySelector('#transformed');
-
-  ImageData pixels = new ColorPalette(img).transformImage();
-
-  canvas.width = pixels.width;
-  canvas.height = pixels.height;
-  canvas.getContext('2d') as CanvasRenderingContext2D
-      ..putImageData(pixels, 0, 0);
-}
-
-void processPalette(ImageElement img) {
-  var results_el = querySelector('#results');
-
-  var sortedColors = sortColorMapByRGB( new ColorPalette(img).getColors() );
-
-  // sortedColorMapKeys(colors).forEach( (key) {
-  sortedColors.forEach( (value) {
-    var color_el = new LIElement();
-    var rgb = value['rgb'];
-    var count = value['count'];
-    var countLabel = new SpanElement();
-    countLabel.appendText(count.toString());
-
-    color_el.style.backgroundColor = "rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 1.0)";
-
-
-    color_el.children.add(countLabel);
-
-    results_el.children.add(color_el);
-  });
 }
---
+++ main.dart
@@ -0,0 +1,57 @@
+import 'color-palette.dart';
+
+import 'dart:html';
+
+void main() {
+  var img = querySelector('.orig');
+  window.onLoad.listen((e) {
+    processPalette(img);
+  });
+}
+
+List sortColorMapByRGB(Map colorMap) {
+  var list = colorMap.values.toList();
+  list.sort( (x, y) => x['rgb'].reduce((a,b) => a + b).compareTo(y['rgb'].reduce((a,b) => a + b)));
+
+  return list;
+}
+
+List sortColoMapByFrequency(Map colorMap) {
+  var list = colorMap.values.toList();
+  list.sort( (x, y) => x['count'].compareTo(y['count']));
+
+  return list;
+}
+
+void transformImage(ImageElement img) {
+  CanvasElement canvas = querySelector('#transformed');
+
+  ImageData pixels = new ColorPalette(img).transformImage();
+
+  canvas.width = pixels.width;
+  canvas.height = pixels.height;
+  canvas.getContext('2d') as CanvasRenderingContext2D
+      ..putImageData(pixels, 0, 0);
+}
+
+void processPalette(ImageElement img) {
+  var results_el = querySelector('#results');
+
+  var sortedColors = sortColorMapByRGB( new ColorPalette(img).getColors() );
+
+  // sortedColorMapKeys(colors).forEach( (key) {
+  sortedColors.forEach( (value) {
+    var color_el = new LIElement();
+    var rgb = value['rgb'];
+    var count = value['count'];
+    var countLabel = new SpanElement();
+    countLabel.appendText(count.toString());
+
+    color_el.style.backgroundColor = "rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 1.0)";
+
+
+    color_el.children.add(countLabel);
+
+    results_el.children.add(color_el);
+  });
+}
